798a27fe32ac450d9593ae7f9918fbdd6cf23cc5
Bas Dijkstra <bas@ontestautomation.com>
2021-06-05 09:32:06 +0200

    Functions, not methods

--- examples/writing_and_running_a.test.js
+++ examples/writing_and_running_a.test.js
@@ -1,19 +1,19 @@
 /**
- * Because this test uses the 'test' method,
+ * Because this function uses the 'test' function,
  * Jest identifies it as a test.
  */
 test('this function is identified as a test', () => {
 });
 
 /**
- * This method, too, uses the 'test' method,
+ * This function, too, uses the 'test' function,
  * and is therefore identified by Jest as a test.
  */
 test('this function is also identified as a test', () => {
 });
 
 /**
- * This is a regular JavaScript method, which will not be identified
+ * This is a regular JavaScript function, which will not be identified
  * as a test by Jest.
  */
 function thisFunctionIsNotIdentifiedAsATest() {
